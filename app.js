ab746a50168add44fd0bb08e207c6d829183ac69
CadinhoVerde <rafaelalexdk@gmail.com>
2024-07-18 17:55:32 -0300

    nav-bar responsiva

---
+++ app.js
@@ -0,0 +1,4 @@
+const menuMobile = document.querySelector(".menu-mobile");
+const nav = document.querySelector(".nav-header")
+
+menuMobile.addEventListener("click", () => nav.classList.toggle("active"));
